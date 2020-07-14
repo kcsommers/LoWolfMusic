46679a8b32491976e38af71e751fcc3a103ccbc4
Kacy Sommers <kacy@dev.adaptiva.com>
2020-07-13 17:29:15 -0700

    adding sitemap

--- lowolf-server/dist/index.js
+++ lowolf-server/dist/index.js
@@ -9,11 +9,16 @@ const port = process.env.PORT || 3000;
 const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
 const body_parser_1 = __importDefault(require("body-parser"));
+const path_1 = __importDefault(require("path"));
 app.use(cors_1.default());
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use('/instagram', require('./controllers/instagram.router'));
 app.use('/photos', require('./controllers/photos.router'));
 app.use('/contact', require('./controllers/contact.router'));
+app.get('/sitemap', (req, res) => {
+    console.log('Hit sitemap');
+    res.sendFile(path_1.default.join(__dirname, '../sitemap.xml'));
+});
 app.listen(port, () => console.log(`Hooked on ${port}`));
 //# sourceMappingURL=index.js.map
--- lowolf-server/src/index.ts
+++ lowolf-server/src/index.ts
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 import cors from 'cors';
 const app = express();
 import bp from 'body-parser';
+import path from 'path';
 
 app.use(cors());
 app.use(bp.json());
@@ -11,5 +12,10 @@ app.use(bp.urlencoded({ extended: true }));
 app.use('/instagram', require('./controllers/instagram.router'));
 app.use('/photos', require('./controllers/photos.router'));
 app.use('/contact', require('./controllers/contact.router'));
+
+app.get('/sitemap', (req: express.Request, res: express.Response) => {
+  console.log('Hit sitemap')
+  res.sendFile(path.join(__dirname, '../sitemap.xml'));
+});
 
 app.listen(port, () => console.log(`Hooked on ${port}`));
