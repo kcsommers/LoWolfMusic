f3eacd8077df3ddd9a59b38a11db21031f9b101c
Kacy Sommers <kacysommers@Kacys-MacBook-Pro.local>
2020-10-18 22:59:04 -0700

    instagram feed service

---
+++ lowolf-client/src/app/components/instagram-feed/instagram-feed-service.ts
@@ -0,0 +1,68 @@
+import { Injectable, OnDestroy, Inject } from '@angular/core';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { take } from 'rxjs/operators';
+
+export interface InstagramImage {
+  id: string;
+  username: string;
+  media_url: string;
+  permalink: string;
+  caption: string;
+}
+
+export interface InstagramResponse {
+  data: InstagramImage[];
+  paging: {
+    cursors: {
+      before: string;
+      after: string;
+    };
+    next: string;
+  };
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InstagramFeedService implements OnDestroy {
+
+  private _imageCache = [];
+
+  private _images$ = new ReplaySubject<InstagramImage[]>(null);
+
+  public error$ = new BehaviorSubject(false);
+
+  private _refreshInterval: number;
+
+  public get images$(): ReplaySubject<InstagramImage[]> {
+    if (!this._imageCache.length) {
+      this.getImages();
+    }
+    return this._images$;
+  }
+
+  constructor(private _http: HttpClient, @Inject('ENVIRONMENT') private environment: any) {
+    this._refreshInterval = window.setInterval(this.getImages.bind(this), 600000);
+  }
+
+  ngOnDestroy() {
+    window.clearInterval(this._refreshInterval);
+  }
+
+  private getImages() {
+    this._http.get<InstagramResponse>(`${this.environment.apiUrl}/instagram`)
+      .pipe(take(1))
+      .subscribe(
+        (res: InstagramResponse) => {
+          this._images$.next(res.data);
+          this._imageCache = res.data;
+        },
+        err => {
+          if (!this._imageCache.length) {
+            this.error$.next(true);
+          }
+        }
+      );
+  }
+}
--- lowolf-client/src/app/components/instagram-feed/instagram-feed.component.ts
+++ lowolf-client/src/app/components/instagram-feed/instagram-feed.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy, HostListener } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { InstagramImage, InstagramFeedService } from './instagram-feed-service';
 
 @Component({
   selector: 'lo-instagram-feed',
@@ -6,11 +9,43 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./instagram-feed.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class InstagramFeedComponent implements OnInit {
+export class InstagramFeedComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public images$ = new BehaviorSubject<InstagramImage[]>(null);
+
+  private _unsubscribe = new Subject();
+
+  public indexLimit = 8;
+
+  constructor(public instagramService: InstagramFeedService) { }
 
   ngOnInit() {
+    this.setIndexLimit();
+    this.instagramService.images$
+      .pipe(takeUntil(this._unsubscribe))
+      .subscribe(imgs => this.images$.next(imgs.slice(0, 8)));
+  }
+
+  ngOnDestroy() {
+    this._unsubscribe.next(false);
+    this._unsubscribe.complete();
+  }
+
+  private setIndexLimit(): void {
+    if (window.innerWidth >= 812) {
+      this.indexLimit = 8;
+      return;
+    }
+    if (window.innerWidth >= 560) {
+      this.indexLimit = 6;
+      return;
+    }
+    this.indexLimit = 4;
+  }
+
+  @HostListener('window:resize')
+  public onResize(): void {
+    this.setIndexLimit();
   }
 
 }
